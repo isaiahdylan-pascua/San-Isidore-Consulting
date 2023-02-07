ce346222838e539f7ef8967e8b2f8369618c6803
isaiahdylan-pascua <isaiah.pascua@obf.ateneo.edu>
2023-02-07 15:07:04 +0800

    Update models.py

--- SANISIDORE/products/models.py
+++ SANISIDORE/products/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+class Product(models.Model):
+    ProductID = models.IntegerField(primary_key=True, max_length= 6, default=1000) 
+    ProductName = models.CharField(max_length=30)
+    ProductCost = models.FloatField(max_length=6)
+    objects = models.Manager()
+
+    def getID(self):
+        return self.ProductID
+
+    def getName(self):
+        return self.ProductName
+    
+    def getCost(self):
+        return self.ProductCost
+
+
+
+
+
