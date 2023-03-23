f44d38b1867367a5e6f91b889700c246bae2af29
SageDesi <sage.desiderio@obf.ateneo.edu>
2023-03-23 15:15:38 +0800

    migrated

--- SANISIDORE/products/models.py
+++ SANISIDORE/products/models.py
@@ -50,11 +50,11 @@ class Order(models.Model):
     def __str__(self):
         return str(self.pk) + str(self.Date) + str(self.Server) + str(self.Table) + str(self.PaymentOption) + str(self.PWDS)
 
-class orderline(models.Model):
-    OrderID = models.ForeignKey('Order',on_delete=models.CASCADE)
-    ProductID = models.ForeignKey('Product',on_delete=models.CASCADE)
-    ProductQty = models.IntegerField(null=False, blank=False)
-    Discount = models.BooleanField()
-    objects = models.Manager()
+# class orderline(models.Model):
+#     OrderID = models.ForeignKey('Order',on_delete=models.CASCADE)
+#     ProductID = models.ForeignKey('Product',on_delete=models.CASCADE)
+#     ProductQty = models.IntegerField(null=False, blank=False)
+#     Discount = models.BooleanField()
+#     objects = models.Manager()
 
     
